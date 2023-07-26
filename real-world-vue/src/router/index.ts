20cef531afc505d1bedbab2d28ff1bac875e639c
yadalaa <yada_la@cmu.ac.th>
2023-07-27 00:24:45 +0700

    finished work

--- real-world-vue/src/router/index.ts
+++ real-world-vue/src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
+import OrganizerListView from '../views/OrganizerListView.vue'
+import StudentView from '../views/StudentView.vue'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'event-list',
+      component: EventListView
     },
     {
       path: '/about',
@@ -15,7 +20,18 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      // component: () => import('../views/AboutView.vue')
+      component: AboutView
+    },
+    {
+      path: '/organizer',
+      name: 'organizer-list',
+      component: OrganizerListView
+    },
+    {
+      path: '/student',
+      name: 'student-info',
+      component: StudentView
     }
   ]
 })
